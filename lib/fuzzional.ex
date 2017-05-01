febe1435045f0b2f7f7b1f8621f17f0a6aa54588
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-05-01 01:10:47 -0400

    initial commit

---
+++ lib/fuzzional.ex
@@ -0,0 +1,18 @@
+defmodule Fuzzional do
+  @moduledoc """
+  Documentation for Fuzzional.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fuzzional.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fuzzional_test.exs
@@ -0,0 +1,8 @@
+defmodule FuzzionalTest do
+  use ExUnit.Case
+  doctest Fuzzional
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
